7b1e36b0b55740b3e657225dae2cc71c4bfdf035
Smit Prajapati <smit51680@gmail.com>
2026-08-30 16:05:27 +0530

    hotfix

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser } from "../API/user";
-import { UpdateProfileImg, UpdateCoverImg, getAllUsers } from "../Actions/user";
+import { UpdateProfileImg, UpdateCoverImg } from "../Actions/user";
 import Card from "../components/Card";
 import { UpdateBio } from "../Actions/user";
 import { FollowUser, UnFollowUser } from "../Actions/user";
